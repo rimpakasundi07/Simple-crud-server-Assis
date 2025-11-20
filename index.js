c396d6fc25f9ca0319ead9831118c2aa15a737ca
rimpa kasundi <rimpakasundi@gmail.com>
2025-11-20 18:09:06 +0530

    Initial commit: Express CRUD server with MongoDB

--- index.js
+++ index.js
@@ -56,6 +56,21 @@ async function run() {
       res.send(result);
     });
 
+    app.patch("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedUser = req.body;
+      const query = { _id: new ObjectId(id) };
+      const update = {
+        $set: {
+          name: updatedUser.name,
+          email: updatedUser.email,
+        },
+      };
+      const options = {};
+      const result = await usersCollection.updateOne(query, update, options);
+      res.send(result);
+    });
+
     app.delete("/users/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
